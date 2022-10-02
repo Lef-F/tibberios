9bde87b1c68ad830abda932a82a8692a2a435486
Lef Filippakis <Lef-F@users.noreply.github.com>
2022-10-02 13:06:14 +0200

    it's aliiiiiiive

--- README.md
+++ README.md
@@ -1,17 +1,60 @@
-# Tibberios
+# Tibberios 🏛
 
-The greek is playing around with Tibber.
+The Greek is playing around with Tibber.
 
-## Config
+Store, update and visualize your own Tibber data from your own `sqlite` database.
+
+[Click here to get your own Tibber invite from your fellow Greek μαλάκα!](https://invite.tibber.com/o10520f3)
+
+## But why? 👀
+
+I really wanted to store some of my Tibber data in a database I control to achieve the following goals:
+
+- Keep a copy of my long-term history of hourly consumption handy
+- Allow me to perform all sorts of neat analytical SQL queries on it
+- Allow me to visualize it in my own tools of preference e.g. Grafana
+- Use it in my own application e.g. e-paper display that shows a graph of today's (and tomorrow, once available) spot prices
+
+## Setup 🛠
+
+First setup your environment using: `poetry install` or `pip install -r requirements.txt`.
 
 You need the following `config.json` file in your project root:
 
 ```json
 {
     "TIBBER_API_KEY": "some_gsdgri_strange_r3f4ww_looking_f43erke_key_from_tibber_dev",
+    "DATABASE_PATH": "/some/path/tibber.db",
 }
 ```
+
+Get your [Tibber API key](https://developer.tibber.com/).
+
+## Run 🏃‍♀️
+
+(Hopefully) you will find all you need in the attached Python help:
+
+```shell
+python run_tibber.py --help
+```
+
+### Get your consumption data and store them in a `sqlite` database file
+
+The following will get you the latest 1000 hours of your data:
+
+```shell
+python run_tibber.py --resolution HOURLY --records 1000
+```
+
+I recommend you run with a lot of records the first time, in order to populate your past data.
+Then you can run every hour and update the last ~48 records, or less if you have a live electricity meter.
+In my case (not live) the consumption data appears with 1-2 days of delay, thus I need to re-fetch the past data in order to keep everything updated.
 
 ## Further reading
 
 - [Tibber Dev Docs](https://developer.tibber.com/)
+- [pyTibber](https://github.com/Danielhiversen/pyTibber)
+
+## Referral copy/pasted from my Tibber App
+
+Hey! 🤗 Here’s a tip! You can save money, save the environment, and avoid unnecessary energy consumption at the same time. How? With Tibber’s hourly energy prices, I can easily move my consumption to the cheapest hours, and avoid expensive hours. Try it out, and we’ll both get 500 kr as a bonus to use in the Tibber Store. ⚡ [Read more here](https://invite.tibber.com/o10520f3)
--- run_tibber.py
+++ run_tibber.py
@@ -1,7 +1,11 @@
 #!/bin/python3
-import tibber
 from dataclasses import dataclass
 from typing import Iterable
+
+import tibber
+from aiohttp import ClientSession
+
+from pprint import pprint
 
 
 @dataclass
@@ -29,22 +33,20 @@ class History:
 class TibberConnector:
     def __init__(self, tibber_api_key: str) -> None:
         self._access_token = tibber_api_key
-        self._connection = tibber.Tibber(self._access_token)
 
     async def get_history(self, resolution: str, records: int) -> Iterable[dict]:
         # TODO: Allow for multiple homes
-        async with self._connection.update_info() as info:
-            self.home = info.get_homes()[0]
-        async with self.home.get_historic_data(
-            n_data=records,
-            resolution=resolution,
-        ) as data:
-            history = data
-        async with self._connection.close_connection() as closed:
-            closed
+        async with ClientSession() as session:
+            tibber_connection = tibber.Tibber(self._access_token, websession=session)
+            await tibber_connection.update_info()
+            home = tibber_connection.get_homes()[0]
 
-        print(f"Got {len(history)} records with resolution: {resolution}")
-        return history
+            self.history = await home.get_historic_data(
+                n_data=records,
+                resolution=resolution,
+            )
+        print(f"Got {len(self.history)} records with resolution: {resolution}")
+        return self.history
 
 
 class Database:
@@ -54,9 +56,12 @@ class Database:
         self._database_path = filename
         self.connection = connect(database=self._database_path)
 
+    def __del__(self) -> None:
+        self.close()
+
     def create_table(self) -> None:
         # TODO: Make generic
-        create_table = """
+        query = """
             CREATE TABLE IF NOT EXISTS consumption(
                 start_time DATE PRIMARY KEY,
                 unit_price REAL,
@@ -65,11 +70,25 @@ class Database:
                 consumption REAL
             );
         """
-        self.connection.execute(create_table)
+        cursor = self.connection
+        cursor = cursor.execute(query)
+        self.connection.commit()
+
+    def show_latest_data(self, limit: int = 10) -> None:
+        # TODO: Make generic
+        query = f"""
+            SELECT *
+            FROM consumption
+            ORDER BY start_time DESC
+            LIMIT {limit};
+        """
+        cursor = self.connection
+        cursor = cursor.execute(query)
+        pprint(cursor.fetchall())
 
     def upsert_table(self, values: Iterable[tuple]) -> None:
         # TODO: Make generic
-        upsert_table = """
+        query = """
             INSERT INTO consumption(
                 start_time
                 , unit_price
@@ -85,29 +104,54 @@ class Database:
                 , consumption = excluded.consumption;
         """
 
-        self.connection.executemany(upsert_table, values)
+        cursor = self.connection
+        cursor.executemany(query, values)
+        self.connection.commit()
+
+    def close(self) -> None:
+        self.connection.close()
 
 
-def main(db_path: str, config_path: str, resolution: str, records: int) -> None:
-    print(db_path, config_path, resolution, records)
+async def main(
+    db_path: str,
+    config_path: str,
+    resolution: str,
+    records: int,
+    verbose: bool,
+) -> None:
     config = Config(filepath=config_path)
 
     if db_path:
         pass
     else:
         db_path = config.database_path
+
+    if verbose:
+        print(f"The database path is: {db_path}")
+        print(f"The config path is: {config_path}")
+        print(f"Fetching {records} in {resolution} resolution")
+
     db = Database(db_path)
     tib = TibberConnector(config.tibber_api_key)
-    history = tib.get_history(resolution=resolution, records=records).close()
+    history = await tib.get_history(resolution=resolution, records=records)
     data = History(data=history)
 
     db.create_table()
+    if verbose:
+        print("Consumption table created")
     db.upsert_table(values=data.values())
+    if verbose:
+        print("Consumption values upserted")
+        print("Latest 10 consumption values:")
+        db.show_latest_data()
+    db.close()
 
 
 if __name__ == "__main__":
-    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+    from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+    from asyncio import new_event_loop
     from os import getcwd, path
+    from sys import exit
 
     parser = ArgumentParser(
         formatter_class=ArgumentDefaultsHelpFormatter,
@@ -117,6 +161,7 @@ if __name__ == "__main__":
         nargs=1,
         required=False,
         help="The path where the SQLite database file is/will be stored. Can be set in DATABASE_PATH key in your config file.",
+        default=path.join(getcwd(), "tibber.db"),
     )
     parser.add_argument(
         "--config-path",
@@ -127,24 +172,36 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--resolution",
-        nargs=1,
+        type=str,
         required=False,
         default="HOURLY",
         help="The resolution to fetch records from the Tibber API. (HOURLY, DAILY, WEEKLY, MONTHLY)",
     )
     parser.add_argument(
         "--records",
-        nargs=1,
-        required=False,
         type=int,
+        required=False,
         default=24 * 2,
         help="The number of latest records to fetch from now to the past from the Tibber API.",
     )
+    parser.add_argument(
+        "--verbose",
+        required=False,
+        default=False,
+        action="store_true",
+        help="Show more information from the process.",
+    )
     args = parser.parse_args()
 
-    main(
-        db_path=args.db_path,
-        config_path=args.config_path,
-        resolution=args.resolution,
-        records=args.records,
+    loop = new_event_loop()
+    loop.run_until_complete(
+        main(
+            db_path=args.db_path,
+            config_path=args.config_path,
+            resolution=args.resolution,
+            records=args.records,
+            verbose=args.verbose,
+        )
     )
+    loop.close()
+    exit(0)
