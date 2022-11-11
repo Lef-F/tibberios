39f73b356a3e4cc303851a719638de9864d9ae28
Lef Filippakis <Lef-F@users.noreply.github.com>
2022-11-11 21:56:50 +0100

    refactor: tests to use the DbTable struct

--- tibberios/tests/test_database.py
+++ tibberios/tests/test_database.py
@@ -2,22 +2,24 @@ import unittest
 from os import path
 from sqlite3 import OperationalError
 
-from tibberios.core import Database
+from tibberios.core import Database, DbTable
 
 
 class TestDatabaseMethods(unittest.TestCase):
     def setUp(self):
         self.db = Database("test_tibberios.db")
-        self.name = "test"
-        self.cols_n_types = {
-            "col1": "REAL PRIMARY KEY",
-            "col2": "REAL",
-            "col3": "REAL",
-        }
-        self.columns = self.cols_n_types.keys()
+        self.table = DbTable(
+            name="test",
+            columns={
+                "col1": "REAL PRIMARY KEY",
+                "col2": "REAL",
+                "col3": "REAL",
+            },
+            pk="col1",
+            order="col1",
+        )
         self.values = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
         self.new_values = [(1, 3, 4), (4, 6, 7), (7, 9, 9)]
-        self.pk = "col1"
 
     def test_database_connection(self):
         # make sure the database connection works
@@ -31,21 +33,26 @@ class TestDatabaseMethods(unittest.TestCase):
 
     def test_database_operations(self):
         # table creation
-        self.db.create_table(name=self.name, cols_n_types=self.cols_n_types)
-        results = self.db.get_latest_data(name=self.name, order=self.pk)
+        self.db.create_table(name=self.table.name, cols_n_types=self.table.columns)
+        results = self.db.get_latest_data(name=self.table.name, order=self.table.pk)
         # table should be empty
         self.assertEqual(len(results), 0)
 
         # add data to table
-        self.db.insert_table(name=self.name, columns=self.columns, values=self.values)
-        results = self.db.get_latest_data(name=self.name, order=self.pk)
+        self.db.insert_table(
+            name=self.table.name, columns=self.table.column_names, values=self.values
+        )
+        results = self.db.get_latest_data(name=self.table.name, order=self.table.pk)
         self.assertEqual(len(self.values), len(results))
 
         # upsert table
         self.db.upsert_table(
-            name=self.name, columns=self.columns, values=self.values, pk=self.pk
+            name=self.table.name,
+            columns=self.table.column_names,
+            values=self.values,
+            pk=self.table.pk,
         )
-        results = self.db.get_latest_data(name=self.name, order=self.pk)
+        results = self.db.get_latest_data(name=self.table.name, order=self.table.pk)
         # table should have values
         self.assertEqual(len(results), len(self.values))
 
